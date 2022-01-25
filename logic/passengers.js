df462115dc1d1621fffaf0e738c4884f116c59a1
markadiusch <hudzik.m@icloud.com>
2022-01-25 21:45:35 +0100

    Update passengers.js

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,17 @@
 function Passengers() {
+    function checkFlightCapacity(capacity, passengersArray){
+        let totalPassengers = 0;
+        let passengers;
+        for(passengers of passengersArray){
+            totalPassengers += passengers;
+        }
 
+        if (totalPassengers<=capacity){
+            return totalPassengers;
+        } else {
+            throw new Error("The number of passengers is more then the capacity.");
+        }
+    };
 }
 
+module.exports = Passengers();
