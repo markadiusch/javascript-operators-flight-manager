05901643ed692609f10dfcc718d1743da6780912
markadiusch <hudzik.m@icloud.com>
2022-01-25 20:33:04 +0100

    a
    a

--- logic/flights.js
+++ logic/flights.js
@@ -2,6 +2,14 @@ function Flights() {
     let flights;
     function calculateNumberOfFlights(passengers, capacity){
 
+        if ((passengers < 0 ) || (!Number.isInteger(Number(passengers)))){
+            throw new Error("The number of passengers must be a positive integer value");
+        }
+
+        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))){
+            throw new Error("The capacity of the flight must be a positive integer value");
+        }
+
         if (passengers % capacity == 0){
             flights = passengers/capacity;
         } else {
