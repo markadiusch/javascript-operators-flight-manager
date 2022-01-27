82c93091f1ad7fe2a21a00e0168e9df823de3c3f
markadiusch <hudzik.m@icloud.com>
2022-01-27 19:21:30 +0100

    Update util.js

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,10 @@
 function Util() {
-
+    function calculateTotalDistributedPassengers(distributedPassengers){
+        let totalNumOfPassengers = 0;
+        totalNumOfPassengers = distributedPassengers.vipPassengersWithBusinessSeats + distributedPassengers.vipPassengersWithEconomySeats + distributedPassengers.regularPassengersWithBusinessSeats + distributedPassengers.regularPassengersWithEconomySeats;
+        
+    };
+    return {calculateTotalDistributedPassengers};
 }
 
+module.exports = Util();
