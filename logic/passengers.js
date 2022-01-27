7c976b2b8500b2d9a2ea31cb4d29aac670786974
markadiusch <hudzik.m@icloud.com>
2022-01-27 17:50:33 +0100

    Update passengers.js

--- logic/passengers.js
+++ logic/passengers.js
@@ -10,6 +10,34 @@ function Passengers() {
         } 
         return passengersNumber;
     }
+
+    function distributeAllSeatsToAllPassengers(vipPassengers, normalPassengers, flights, businessSeats, economySeats){
+        let passengers;
+        let distribution = {
+            vipPassengersWithBusinessSeats: 0,
+            vipPassengersWithEconomySeats: 0,
+            regularPassengersWithBusinessSeats: 0,
+            regularPassengersWithEconomySeats: 0
+        };
+
+        for(passengers of vipPassengers){
+            if (businessSeats>0){
+                vipPassengersWithBusinessSeats += passengers;
+            } else if (economySeats>0) {
+                vipPassengersWithEconomySeats += passengers;
+            }
+        }
+
+        for(passengers of normalPassengers){
+            if (businessSeats>0){
+                regularPassengersWithBusinessSeats =+ passengers;
+            } else if (economySeats>0){
+                regularPassengersWithEconomySeats =+ passengers;
+            }
+        }
+
+        return {distributeAllSeatsToAllPassengers}; 
+    }
     return {checkFlightCapacity};
 };
 
