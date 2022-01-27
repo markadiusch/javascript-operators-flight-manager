4043fc66efaa6358fe6b894feaa6413708a106f5
markadiusch <hudzik.m@icloud.com>
2022-01-27 20:10:13 +0100

    Update price.js

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,14 @@
 function Prices() {
+    function calculateFinalPrice(basePrice, passengerVariation, flightVariation){
+        let finalPrice = 0;
 
+        finalPrice = basePrice + passengerVariation * basePrice + flightVariation * (basePrice - passengerVariation * basePrice);
+
+        return finalPrice.toFixed(2);
+    }
+    return {calculateFinalPrice()};
 }
+
+module.exports = Prices ();
+
 
